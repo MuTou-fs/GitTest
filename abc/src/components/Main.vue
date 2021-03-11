94f96b48759a7884e296a30f3e22ffafaf833f47
mutou_fs <mutou_fs@163.com>
2021-03-11 13:59:32 +0800

    第一次提交

---
+++ abc/src/components/Main.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    *****
+    {{ word.word_id }}
+    <Button @click="updateWord">UpdateWord</Button>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+
+  data () {
+    this.getWord();
+    return {
+      msg: "",
+      word: {
+        word_id: "",
+        trans_1: ""
+      },
+      wordList: [
+
+      ]
+    }
+  },
+  methods: {
+    getWord() {
+      this.axios.get('/testBoot/getAllWord').then((response)=>{
+        this.word = response.data[0];
+      }).catch((response)=>{
+        console.log("this is catch");
+        console.log(response);
+      })
+    },
+    updateWord() {
+      this.axios.get('/testBoot/updateWord').then((response)=>{
+        this.msg = response.data;
+        console.log(response);
+      }).catch((response)=>{
+        console.log("this is catch");
+        console.log(response);
+      })
+
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ abc/src/components/gittest.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    第一次修改
+    第二次修改
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
