3cdba43e106952ea4ab809184d242dd07e505d11
mutou_fs <54254647+MuTou-fs@users.noreply.github.com>
2021-03-15 00:04:55 +0800

    。。。”

--- abc/src/App.vue
+++ abc/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <myHeader></myHeader>
-    <myContent></myContent>
-    <myFooter></myFooter>
+    <Layout>
+      <myHeader></myHeader>
+      <myContent></myContent>
+      <Divider :style="{color: red}"></Divider>
+      <myFooter></myFooter>
+    </Layout>
 
     <!-- <router-view/> -->
   </div>
@@ -21,7 +24,33 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   height: 100%;
+}
+.ivu-layout {
+  height: 100%;
+  align-items: center;
+  background-color: rgb(140, 245, 196);
+}
+
+.ivu-layout-header,.ivu-layout-content,.ivu-layout-footer {
+  margin: 0;
+  border: 0;
+  padding: 0;
+  width: 1200px;
+  text-align: center;
+}
+.ivu-layout-header {
+ 
+}
+.ivu-layout-content {
+
+}
+
+.ivu-layout-footer {
+  height: 54px;
+}
+
+.ivu-divider{
+ padding: 1.5px;
 }
 </style>
